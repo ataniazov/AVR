a8ff19614ac49e65d45cb565b8ef4a2b6abe7ceb
Ata Niyazov <ata.niazov@gmail.com>
2019-12-18 01:13:40 +0300

    added jmp project

---
+++ AVR Studio/jmp/AvrBuild.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\Users\delta\Documents\jmp\labels.tmp" -fI -W+ie -C V2E -o "C:\Users\delta\Documents\jmp\jmp.hex" -d "C:\Users\delta\Documents\jmp\jmp.obj" -e "C:\Users\delta\Documents\jmp\jmp.eep" -m "C:\Users\delta\Documents\jmp\jmp.map" "C:\Users\delta\Documents\jmp\jmp.asm"
